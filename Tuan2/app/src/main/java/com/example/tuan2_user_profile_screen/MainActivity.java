05e92b545c92943c8e3701852a571d3997d02310
Tuan Le <tuan.cvng1720@gmail.com>
2022-09-10 21:45:31 +0700

    Tuan 2 - User profile Sceen

---
+++ Tuan2/app/src/main/java/com/example/tuan2_user_profile_screen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuan2_user_profile_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
